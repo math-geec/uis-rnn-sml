10b68310cc917368e0cf890c829da1d69527b6ce
math-geec <victordean087@gmail.com>
2020-06-21 00:18:24 +0100

    First local attempt success.

--- demo.py
+++ demo.py
@@ -17,17 +17,17 @@ import numpy as np
 from functools import partial
 from torch.utils.tensorboard import SummaryWriter
 import torch.multiprocessing as mp
+
 mp = mp.get_context('forkserver')
 
 import uisrnn
-
 
 SAVED_MODEL_NAME = 'saved_model.uisrnn'
 NUM_WORKERS = 2
 
 
 def diarization_experiment(model_args, training_args, inference_args):
-  """Experiment pipeline.
+    """Experiment pipeline.
 
   Load data --> train model --> test model --> output result
 
@@ -36,65 +36,65 @@ def diarization_experiment(model_args, training_args, inference_args):
     training_args: training configurations
     inference_args: inference configurations
   """
-  # data loading
-  train_data = np.load('./data/toy_training_data.npz', allow_pickle=True)
-  test_data = np.load('./data/toy_testing_data.npz', allow_pickle=True)
-  train_sequence = train_data['train_sequence']
-  train_cluster_id = train_data['train_cluster_id']
-  test_sequences = test_data['test_sequences'].tolist()
-  test_cluster_ids = test_data['test_cluster_ids'].tolist()
+    # data loading
+    train_data = np.load('./data/toy_training_data.npz', allow_pickle=True)
+    test_data = np.load('./data/toy_testing_data.npz', allow_pickle=True)
+    train_sequence = train_data['train_sequence']
+    train_cluster_id = train_data['train_cluster_id']
+    test_sequences = test_data['test_sequences'].tolist()
+    test_cluster_ids = test_data['test_cluster_ids'].tolist()
 
-  # model init
-  model = uisrnn.UISRNN(model_args)
-  # model.load(SAVED_MODEL_NAME) # to load a checkpoint
-  # tensorboard writer init
-  writer = SummaryWriter()
+    # model init
+    model = uisrnn.UISRNN(model_args)
+    # model.load(SAVED_MODEL_NAME) # to load a checkpoint
+    # tensorboard writer init
+    writer = SummaryWriter()
 
-  # training
-  for epoch in range(training_args.epochs):
-    stats = model.fit(train_sequence, train_cluster_id, training_args)
-    # add to tensorboard
-    for loss, cur_iter in stats:
-      for loss_name, loss_value in loss.items():
-        writer.add_scalar('loss/' + loss_name, loss_value, cur_iter)
-    # save the mdoel
-    model.save(SAVED_MODEL_NAME)
+    # training
+    for epoch in range(training_args.epochs):
+        stats = model.fit(train_sequence, train_cluster_id, training_args)
+        # add to tensorboard
+        for loss, cur_iter in stats:
+            for loss_name, loss_value in loss.items():
+                writer.add_scalar('loss/' + loss_name, loss_value, cur_iter)
+        # save the mdoel
+        model.save(SAVED_MODEL_NAME)
 
-  # testing
-  predicted_cluster_ids = []
-  test_record = []
-  # predict sequences in parallel
-  model.rnn_model.share_memory()
-  pool = mp.Pool(NUM_WORKERS, maxtasksperchild=None)
-  pred_gen = pool.imap(
-      func=partial(model.predict, args=inference_args),
-      iterable=test_sequences)
-  # collect and score predicitons
-  for idx, predicted_cluster_id in enumerate(pred_gen):
-    accuracy = uisrnn.compute_sequence_match_accuracy(
-        test_cluster_ids[idx], predicted_cluster_id)
-    predicted_cluster_ids.append(predicted_cluster_id)
-    test_record.append((accuracy, len(test_cluster_ids[idx])))
-    print('Ground truth labels:')
-    print(test_cluster_ids[idx])
-    print('Predicted labels:')
-    print(predicted_cluster_id)
-    print('-' * 80)
+    # testing
+    predicted_cluster_ids = []
+    test_record = []
+    # predict sequences in parallel
+    model.rnn_model.share_memory()
+    pool = mp.Pool(NUM_WORKERS, maxtasksperchild=None)
+    pred_gen = pool.imap(
+        func=partial(model.predict, args=inference_args),
+        iterable=test_sequences)
+    # collect and score predicitons
+    for idx, predicted_cluster_id in enumerate(pred_gen):
+        accuracy = uisrnn.compute_sequence_match_accuracy(
+            test_cluster_ids[idx], predicted_cluster_id)
+        predicted_cluster_ids.append(predicted_cluster_id)
+        test_record.append((accuracy, len(test_cluster_ids[idx])))
+        print('Ground truth labels:')
+        print(test_cluster_ids[idx])
+        print('Predicted labels:')
+        print(predicted_cluster_id)
+        print('-' * 80)
 
-  # close multiprocessing pool
-  pool.close()
-  # close tensorboard writer
-  writer.close()
+    # close multiprocessing pool
+    pool.close()
+    # close tensorboard writer
+    writer.close()
 
-  print('Finished diarization experiment')
-  print(uisrnn.output_result(model_args, training_args, test_record))
+    print('Finished diarization experiment')
+    print(uisrnn.output_result(model_args, training_args, test_record))
 
 
 def main():
-  """The main function."""
-  model_args, training_args, inference_args = uisrnn.parse_arguments()
-  diarization_experiment(model_args, training_args, inference_args)
+    """The main function."""
+    model_args, training_args, inference_args = uisrnn.parse_arguments()
+    diarization_experiment(model_args, training_args, inference_args)
 
 
 if __name__ == '__main__':
-  main()
+    main()
